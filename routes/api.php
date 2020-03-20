76e39a72bcab14661e570f2b241220da9a73b61e
Nikola <nikolaviki@hotmail.com>
2020-03-20 12:24:19 +0100

    Zadatak 3

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('api')->get('/cars', 'CarsController@index');
+Route::middleware('api')->get('/cars/{id}', 'CarsController@show');
+Route::middleware('api')->post('/cars', 'CarsController@store');
+Route::middleware('api')->put('/cars/{id}', 'CarsController@update');
+Route::middleware('api')->delete('/cars/{id}', 'CarsController@destroy');
